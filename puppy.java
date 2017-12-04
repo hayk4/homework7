102b30aa646afce6404e5bf3d47c89ddc6d98b47
hayk4 <31859875+hayk4@users.noreply.github.com>
2017-12-04 23:27:43 +0400

    Add files via upload

---
+++ puppy.java
@@ -0,0 +1,19 @@
+//i dont know about directory :\
+public class Puppy {
+
+	
+	       private String name;
+
+	
+	      public Puppy(String name) {
+		
+		this.name = name;
+	}
+	
+	       public String getName() {
+	
+		return this.name;
+	}
+	
+
+}
---
+++ utilts.java
@@ -0,0 +1,24 @@
+
+ public class Math {
+
+	
+	          public static int factorial(int x) {
+	
+		if(x === 1) return 1;
+		
+	
+		return n*factorial(n-1);
+	
+}
+	
+	
+	public static int factorialLoop(int x) {
+		            int res = 1;
+		        for(int i = 2; i <= x; i++) {
+			                   res *= i;
+		}
+	 
+	 	return res;
+	}
+
+}
